2727a0ece55542377263c90550e36c183282fdde
Fishman <98775292+Fishman16180@users.noreply.github.com>
2022-08-13 20:27:00 +0200

    Add files via upload

---
+++ capicua1.cpp
@@ -0,0 +1,35 @@
+/*################################
+
+    --program made by Jan Peix--
+
+  ################################*/
+
+#include <iostream>
+
+using namespace std;
+
+int number_inverted(int n){
+    int number=0, rest;
+    while (n != 0){
+        rest = n % 10;
+        number = number * 10 + rest;
+        n /= 10;
+    }
+    return number;
+}
+
+int main()
+{
+    int a, count=0, number;
+    cout<<"Insereix-hi un numero enter: ";
+    cin>>a;
+
+    for (number=0; number<=a; number=number+1){
+        if (number==number_inverted(number)){
+            count+=1;
+            cout<<"El numero de capicuas que hi ha en un rang de 0 a "<<a<<" es de: "<<count<<" capicuas"<<endl;
+        }
+    }
+
+    return 0;
+}
---
+++ capicua2.cpp
@@ -0,0 +1,37 @@
+/*################################
+
+    --program made by Jan Peix--
+
+  ################################*/
+
+#include <iostream>
+
+using namespace std;
+
+int number_inverted(int n){
+    int number=0, rest;
+    while (n != 0){
+        rest = n % 10;
+        number = number * 10 + rest;
+        n /= 10;
+    }
+    return number;
+}
+
+int main()
+{
+    int a, b, count=0, number;
+    cout<<"Insereix-hi el numero inicial del rang: ";
+    cin>>a;
+    cout<<"Insereix-hi el numero final del rang: ";
+    cin>>b;
+
+    for (number=a; number<=b; number=number+1){
+        if (number==number_inverted(number)){
+            count+=1;
+            cout<<"El numero de capicuas que hi ha en un rang de "<<a<<" a "<<b<<" es de: "<<count<<" capicuas"<<endl;
+        }
+    }
+
+    return 0;
+}
