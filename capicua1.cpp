a6af1cae4367ee99dc934eb3530e2fd1620098b3
Fishman <98775292+Fishman16180@users.noreply.github.com>
2022-08-14 18:22:46 +0200

    Update capicua1.cpp

--- capicua1.cpp
+++ capicua1.cpp
@@ -21,13 +21,13 @@ int number_inverted(int n){
 int main()
 {
     int a, count=0, number;
-    cout<<"Enter an integer value:: ";
+    cout<<"Enter an integer value: ";
     cin>>a;
 
     for (number=0; number<=a; number=number+1){
         if (number==number_inverted(number)){
             count+=1;
-            cout<<"The number of capicuas that are in a range from 0 to "<<a<<" is of: "<<count<<" capicuas"<<endl;
+            cout<<"The number of palindromic numbers (capicuas) in a range of 0 to "<<a<<" is of: "<<count<<" palindromic numbers (capicuas)"<<endl;
         }
     }
 
